0c6aad994a72e803d3818f1c89aaf47952b795e6
LHN-Gift <LHN-Gift>
2024-07-23 22:33:53 +0000

    Add notebook to create simple Dataframe

---
+++ create_data.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+import pandas as pd
+
+# COMMAND ----------
+
+data = {
+    'name': ['Nhat', 'Tam', 'N. Anh', 'Q. Anh'],
+    'age': [40, 35, 4, 2]
+    'pob': ['Hue', 'Saigon', 'Saigon', 'Saigon']
+}
+
+df = pd.DataFrame(data=data)
+
+df
